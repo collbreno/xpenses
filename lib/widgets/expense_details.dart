0fc157c27ea7f2219db997bb597b7fa650632839
Breno Coll <collbreno@dcc.ufrj.br>
2024-10-19 00:25:00 -0300

    changing expense details screen

--- lib/widgets/expense_details.dart
+++ lib/widgets/expense_details.dart
@@ -1,7 +1,10 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:money2/money2.dart';
 import 'package:xpenses/models/expense_model.dart';
 import 'package:xpenses/models/installment_model.dart';
+import 'package:xpenses/models/person_part_model.dart';
 import 'package:xpenses/utils/date_utils.dart';
 import 'package:xpenses/widgets/tag_chip.dart';
 
@@ -16,17 +19,46 @@ class ExpenseDetails extends StatelessWidget {
         ListTile(title: Text(expense.title)),
         ListTile(
           title: Text(expense.totalValue!.toString()),
-          subtitle: Text('Minha parte: ${expense.myPart!}'),
           trailing: Text(expense.firstDate!.formatDMY()),
         ),
         if (expense.description.isNotEmpty)
           ListTile(title: Text(expense.description)),
-        const Divider(indent: 16, endIndent: 16),
-        for (final installment in expense.installments!)
-          _buildInstallment(installment),
-        const Divider(indent: 16, endIndent: 16),
-        _buildTags(),
+        if (expense.totalValue != expense.myPart) ...[
+          const Divider(indent: 16, endIndent: 16),
+          _buildPersonParts(),
+        ],
+        if (expense.installments!.length != 1) ...[
+          const Divider(indent: 16, endIndent: 16),
+          for (final installment in expense.installments!)
+            _buildInstallment(installment),
+        ],
+        if (expense.tags!.isNotEmpty) ...[
+          const Divider(indent: 16, endIndent: 16),
+          _buildTags(),
+        ],
       ],
+    );
+  }
+
+  Widget _buildPersonParts() {
+    final me = PersonPart(id: 0, value: expense.myPart!, name: 'Eu');
+    final personParts = expense.installments!
+        .fold(<PersonPart>[], (acc, e) => acc + e.personParts)
+        .groupFoldBy<String, Money>(
+            (e) => e.name, (acc, e) => acc == null ? e.value : acc + e.value)
+        .entries
+        .map((e) => PersonPart(id: 0, value: e.value, name: e.key));
+
+    final list = [me, ...personParts];
+    return Column(
+      children: list
+          .map((e) => ListTile(
+                dense: true,
+                minTileHeight: 20,
+                title: Text(e.name),
+                trailing: Text(e.value.toString()),
+              ))
+          .toList(),
     );
   }
 
@@ -51,32 +83,10 @@ class ExpenseDetails extends StatelessWidget {
 
   Widget _buildInstallment(Installment installment) {
     return ListTile(
+      dense: true,
+      minTileHeight: 20,
       title: Text(installment.value.toString()),
-      titleAlignment: ListTileTitleAlignment.top,
       trailing: Text(installment.date.formatDMY()),
-      subtitle: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: installment.personParts
-              .map(
-                (e) => Row(
-                  children: [
-                    Text(
-                      e.name,
-                      style: const TextStyle(fontSize: 12),
-                    ),
-                    const SizedBox(width: 8),
-                    Text(
-                      e.value.toString(),
-                      style: const TextStyle(fontSize: 12),
-                    )
-                  ],
-                ),
-              )
-              .toList(),
-        ),
-      ),
     );
   }
 }
